5f38692f8353f54f1cc0ce62beaef34aa4424b6c
Nicollas <nicollas.kuns@escola.pr.gov.br>
2025-09-13 17:31:25 -0300

    javascript

---
+++ index.js
@@ -0,0 +1,67 @@
+document.getElementById('soma').onclick = function () {
+    let n1 = Number(document.getElementById('numero1').value.trim());
+    let n2 = Number(document.getElementById('numero2').value.trim());
+
+    if (!n1 || !n2) {
+        alert("Digite dois números válidos!");
+        return;
+    }
+
+    let resultado = n1 + n2;
+
+    document.getElementById('numero1').value = '';
+    document.getElementById('numero2').value = '';
+
+    document.getElementById('resultados').innerHTML = `${resultado}`;
+}
+
+document.getElementById('subtracao').onclick = function () {
+    let n1 = Number(document.getElementById('numero1').value.trim());
+    let n2 = Number(document.getElementById('numero2').value.trim());
+
+    if (!n1 || !n2) {
+        alert("Digite dois números válidos!");
+        return;
+    }
+
+    let resultado = n1 - n2;
+
+    document.getElementById('numero1').value = '';
+    document.getElementById('numero2').value = '';
+
+    document.getElementById('resultados').innerHTML = `${resultado}`;
+}
+
+document.getElementById('multiplicacao').onclick = function () {
+    let n1 = Number(document.getElementById('numero1').value.trim());
+    let n2 = Number(document.getElementById('numero2').value.trim());
+
+    if (!n1 || !n2) {
+        alert("Digite dois números válidos!");
+        return;
+    }
+
+    let resultado = n1 * n2;
+
+    document.getElementById('numero1').value = '';
+    document.getElementById('numero2').value = '';
+
+    document.getElementById('resultados').innerHTML = `${resultado}`;
+}
+
+document.getElementById('divisao').onclick = function () {
+    let n1 = Number(document.getElementById('numero1').value.trim());
+    let n2 = Number(document.getElementById('numero2').value.trim());
+
+    if (!n1 || !n2) {
+        alert("Digite dois números válidos!");
+        return;
+    }
+
+    let resultado = n1 / n2;
+
+    document.getElementById('numero1').value = '';
+    document.getElementById('numero2').value = '';
+
+    document.getElementById('resultados').innerHTML = `${resultado}`;
+}
